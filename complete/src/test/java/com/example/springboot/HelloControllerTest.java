a4059be922f1cda93056b70f512affe4396961fd
Clay <abdallahalrif2@gmail.com>
2022-12-15 12:50:26 +0100

    Update HelloControllerTest.java

--- complete/src/test/java/com/example/springboot/HelloControllerTest.java
+++ complete/src/test/java/com/example/springboot/HelloControllerTest.java
@@ -24,6 +24,6 @@ public class HelloControllerTest {
 	public void getHello() throws Exception {
 		mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk())
-				.andExpect(content().string(equalTo("Hallo Herr Bozakov")));
+				.andExpect(content().string(equalTo("Hallo Herr Bozakov!!")));
 	}
 }
